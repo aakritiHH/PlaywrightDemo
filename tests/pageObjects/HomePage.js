3ca0bf7727de4971f20cd2439b5bf2933c1e8de3
Roopam Sharma <roopam.sharma@MACPRO640.local>
2025-02-10 17:05:28 +0530

    region_changes

--- tests/pageObjects/HomePage.js
+++ tests/pageObjects/HomePage.js
@@ -143,14 +143,23 @@ class HomePage {
 
     async changeGeoLocation(){
         await this.page.waitForLoadState('load');
-        const geoLocation = await this.page.locator('#ge_ss0_1 span').textContent();
-        console.log('Location when landed is .......', geoLocation);
+        await expect(this.page.getByRole('button', { name: 'EN' })).toBeVisible();
         await this.page.getByRole('button', { name: 'EN' }).click();
         await expect(this.page.getByText('CloseYour RegionGlobalNorth')).toBeVisible();
         await this.page.getByLabel('Your Region').selectOption('Global');
         await this.page.getByRole('button', { name: 'Set Location' }).click();
         await this.page.getByRole('button', { name: 'EN' }).click();
         await this.page.waitForLoadState('load');
+        await expect(page.locator('#ge_ss769_0').getByText('Shipping to')).toBeVisible();
+        if(await expect(this.page.getByRole('button', { name: 'EN' })).toBeVisible()){
+            await this.page.getByRole('button', { name: 'EN' }).click();
+            await expect(this.page.getByText('CloseYour RegionGlobalNorth')).toBeVisible();
+            await this.page.getByLabel('Your Region').selectOption('Global');
+            await this.page.getByRole('button', { name: 'Set Location' }).click();
+            await this.page.getByRole('button', { name: 'EN' }).click();
+            await this.page.waitForLoadState('load');
+            await expect(page.locator('#ge_ss769_0').getByText('Shipping to')).toBeVisible();
+        }
         const location = await this.page.locator('#ge_ss0_1 span').textContent();
         console.log('Location after .......', location);
         if(location!='India'){
