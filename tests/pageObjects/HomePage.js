0ce7c539ead30bd5c57ff3b5cf02ce44b295ca15
Roopam Sharma <roopam.sharma@MACPRO640.local>
2025-02-10 17:21:56 +0530

    region_changes

--- tests/pageObjects/HomePage.js
+++ tests/pageObjects/HomePage.js
@@ -150,7 +150,7 @@ class HomePage {
         await this.page.getByRole('button', { name: 'Set Location' }).click();
         await this.page.getByRole('button', { name: 'EN' }).click();
         await this.page.waitForLoadState('load');
-        await expect(page.locator('#ge_ss769_0').getByText('Shipping to')).toBeVisible();
+        await expect(this.page.locator('#ge_ss769_0').getByText('Shipping to')).toBeVisible();
         if(await expect(this.page.getByRole('button', { name: 'EN' })).toBeVisible()){
             await this.page.getByRole('button', { name: 'EN' }).click();
             await expect(this.page.getByText('CloseYour RegionGlobalNorth')).toBeVisible();
@@ -158,7 +158,7 @@ class HomePage {
             await this.page.getByRole('button', { name: 'Set Location' }).click();
             await this.page.getByRole('button', { name: 'EN' }).click();
             await this.page.waitForLoadState('load');
-            await expect(page.locator('#ge_ss769_0').getByText('Shipping to')).toBeVisible();
+            await expect(this.page.locator('#ge_ss769_0').getByText('Shipping to')).toBeVisible();
         }
         const location = await this.page.locator('#ge_ss0_1 span').textContent();
         console.log('Location after .......', location);
